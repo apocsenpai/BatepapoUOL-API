bba64d86a80f282462fdafb792622f1d525a33d2
apocsenpai <jonatassennas@ufrrj.br>
2023-01-12 13:20:48 -0300

    feat(loginChat): create post request to allow user logins in chat

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
+import dayjs from "dayjs";
+
+const OK = 200;
+const CREATED = 201;
+const CONFLICT = 409;
+const UNPROCESSABLE = 422;
+const INTERNAL_SERVER_ERROR = 500;
 
 const server = express();
 dotenv.config();
@@ -9,15 +16,38 @@ server.use(cors());
 server.use(express.json());
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
-let db;
 
 try {
   await mongoClient.connect();
 } catch (error) {
   console.log("Erro ao se conectar ao banco de dados", error.message);
 }
+const db = mongoClient.db();
 
-db = mongoClient.db(""); //Adicionar nome do database
-const collection = db.collection(""); // criar collection
+server.post("/participants", async (request, response) => {
+  const { name } = request.body;
+  try {
+    const nameIsAlreadyRegistered = await db
+      .collection("participants")
+      .findOne({ name });
+
+    if (nameIsAlreadyRegistered) {
+      return response.status(CONFLICT).send("Usuário já cadastrado");
+    }
+    const userRegister = { name, lastStatus: Date.now() };
+    await db.collection("participants").insertOne(userRegister);
+    const loginMessage = {
+      from: name,
+      to: "Todos",
+      text: "Entra na sala...",
+      type: "status",
+      time: dayjs().format("HH:mm:ss"),
+    };
+    await db.collection("messages").insertOne(loginMessage);
+    response.sendStatus(CREATED);
+  } catch (err) {
+    return response.status(INTERNAL_SERVER_ERROR).send("Erro no servidor!");
+  }
+});
 
 server.listen(process.env.PORT, () => console.log(process.env.PORT));
